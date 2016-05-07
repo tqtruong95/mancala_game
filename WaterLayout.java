eae270287eb95b34d96089752c938ae28b69d7f1
Tipper Truong <tqtruong95@gmail.com>
2016-05-06 18:22:16 -0700

    added javadocs

--- WaterLayout.java
+++ WaterLayout.java
@@ -2,11 +2,16 @@ import java.awt.Graphics;
 import java.awt.Image;
 import java.io.File;
 import java.io.IOException;
-
 import javax.imageio.ImageIO;
 
+/**
+ * Creating the Water Background Layout 
+ */
 public class WaterLayout extends BoardLayout{
 	private Image background;
+	/**
+	 * Constructing a water theme board
+	 */
 	public WaterLayout(){
 		try {
 			this.background = ImageIO.read(new File("mancala_project/water.jpg"));
@@ -16,6 +21,12 @@ public class WaterLayout extends BoardLayout{
 		}
 	}
 	
+	/**
+	 * Displays the water theme board
+	 * @param g The Graphics object
+	 * @param b The Board object
+	 * @param game the Mancala object
+	 */
 	public void display(Graphics g, Board b, Mancala game){
 		g.drawImage(background, 0, 0, 1100, 600, b);
 		super.display(g, b, game);

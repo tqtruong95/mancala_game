ea55556d88fd5d2568bfc09acf2306c68de8e7bc
nguyenq2006 <nguyenq2006@gmail.com>
2016-05-06 18:08:17 -0700

    Update Board.java

--- Board.java
+++ Board.java
@@ -7,15 +7,28 @@ import javax.swing.JComponent;
 public class Board extends JComponent {
 	private BoardLayout layout;
 	private Mancala game;
+	
+	/**
+	 * Construct a new Mancala board
+	 * @param layout the BoardLayout
+	 */
 	public Board(BoardLayout layout, Mancala game){
 		this.layout = layout;
 		this.game = game;
 	}
 
+	/**
+	 * draw the board
+	 * Graphics g object of Graphics
+	 */
 	public void paintComponent(Graphics g){
 		layout.display(g, this, game);
 	}
 	
+	/**
+	 * get the board layout
+	 * @return board layout
+	 */ 
 	public BoardLayout mancalaLayout(){
 		return layout;
 	}
